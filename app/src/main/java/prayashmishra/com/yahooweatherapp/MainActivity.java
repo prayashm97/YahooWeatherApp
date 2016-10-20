262bd32c6b1974a201cdfc64e680c427fdbbd6b7
Prayash Mishra <pmishra@mail.uoguelph.ca>
2016-10-20 17:02:50 -0400

    update on Network

--- app/src/main/java/prayashmishra/com/yahooweatherapp/MainActivity.java
+++ app/src/main/java/prayashmishra/com/yahooweatherapp/MainActivity.java
@@ -1,27 +1,49 @@
 package prayashmishra.com.yahooweatherapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Toast;
 
 import prayashmishra.com.yahooweatherapp.data.Channel;
-import prayashmishra.com.yahooweatherapp.data.Item;
 import prayashmishra.com.yahooweatherapp.service.WeatherServiceCallback;
-import prayashmishra.com.yahooweatherapp.service.YahooWeatherService;
 
 public class MainActivity extends AppCompatActivity implements WeatherServiceCallback, AdapterView.OnItemClickListener {
+
+    private ProgressDialog dialog;
+
+    private boolean isNetworkAvailable() {
+        ConnectivityManager connectivityManager
+                = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if(isNetworkAvailable()){
+            Toast.makeText(getApplicationContext(),"Internet: yes", Toast.LENGTH_LONG).show();
+        }
+        else {
+            Toast.makeText(getApplicationContext(),"Internet: no", Toast.LENGTH_LONG).show();
+
+
+        }
     }
 
+    @Override
     public void serviceSuccess(final Channel channel) {
 
     }
 
+    @Override
     public void serviceFailure(Exception exception) {
     }
 
