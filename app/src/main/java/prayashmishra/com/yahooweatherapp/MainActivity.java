8d365f894f79eb2ab0f9f8165c8f7eef77a188bd
Prayash Mishra <pmishra@mail.uoguelph.ca>
2016-10-21 15:10:09 -0400

    checks network, dialog box, fixed back button

--- app/src/main/java/prayashmishra/com/yahooweatherapp/MainActivity.java
+++ app/src/main/java/prayashmishra/com/yahooweatherapp/MainActivity.java
@@ -1,10 +1,12 @@
 package prayashmishra.com.yahooweatherapp;
 
-import android.app.ProgressDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.pm.ActivityInfo;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
@@ -15,7 +17,25 @@ import prayashmishra.com.yahooweatherapp.service.WeatherServiceCallback;
 
 public class MainActivity extends AppCompatActivity implements WeatherServiceCallback, AdapterView.OnItemClickListener {
 
-    private ProgressDialog dialog;
+    //If user presses the back button, prompt with a dialog
+    @Override
+    public void onBackPressed() {
+        new AlertDialog.Builder(this)
+                .setIcon(R.mipmap.ic_launcher)
+                .setTitle("Closing Activity")
+                .setMessage("Are you sure you want to exit?")
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        finish();
+                    }
+
+                })
+                .setNegativeButton("No", null)
+                .show();
+    }
+
+    
 
     private boolean isNetworkAvailable() {
         ConnectivityManager connectivityManager
@@ -28,19 +48,29 @@ public class MainActivity extends AppCompatActivity implements WeatherServiceCal
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        if(isNetworkAvailable()){
-            Toast.makeText(getApplicationContext(),"Internet: yes", Toast.LENGTH_LONG).show();
-        }
-        else {
-            Toast.makeText(getApplicationContext(),"Internet: no", Toast.LENGTH_LONG).show();
+        checkInternetDialog();
+    }
 
+    public void checkInternetDialog() {
+        if (isNetworkAvailable()) {
+            Toast.makeText(getApplicationContext(), "Internet: yes", Toast.LENGTH_LONG).show();
+        } else {
+            new AlertDialog.Builder(this)
+                    .setTitle("No internet connection")
+                    .setMessage("Check your internet and try again")
+                    .setPositiveButton("Okay", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            finish();
+                        }
 
+                    })
+                    .show();
         }
     }
 
     @Override
     public void serviceSuccess(final Channel channel) {
-
     }
 
     @Override
@@ -49,6 +79,5 @@ public class MainActivity extends AppCompatActivity implements WeatherServiceCal
 
     @Override
     public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-
     }
 }
