95f0b4d2cf7b159a2e9d14f89fa4412be2d1f857
Prayash Mishra <pmishra@mail.uoguelph.ca>
2017-01-09 15:48:18 -0500

    update on serviceSuccess and onCreate

--- app/src/main/java/prayashmishra/com/yahooweatherapp/MainActivity.java
+++ app/src/main/java/prayashmishra/com/yahooweatherapp/MainActivity.java
@@ -2,44 +2,44 @@ package prayashmishra.com.yahooweatherapp;
 
 import android.content.Context;
 import android.content.DialogInterface;
+import android.graphics.drawable.Drawable;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import prayashmishra.com.yahooweatherapp.data.Channel;
-import prayashmishra.com.yahooweatherapp.service.WeatherServiceCallback;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import prayashmishra.com.yahooweatherapp.data.*;
+import prayashmishra.com.yahooweatherapp.service.*;
 
 public class MainActivity extends AppCompatActivity implements WeatherServiceCallback, AdapterView.OnItemClickListener {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        //checks the internet connection
-        if (internetDialog()) {
-            //if there is internet, does not exit the app
-            //starts the app
-        }
+    private YahooWeatherService service;
 
-    }
+    private static final String LOG_TAG = "Autocomplete";
+    private static final String PLACES_API_BASE = "https://maps.googleapis.com/maps/api/place";
+    private static final String TYPE_AUTOCOMPLETE = "/autocomplete";
+    private static final String OUT_JSON = "/json";
+    private static final String API_KEY = "AIzaSyCEwp4TvdqeCVAKbnYm1qYq0XQhdcF1yEU";
 
+    private ImageView weatherIconImageView;
+    private TextView temperatureTextView;
+    private TextView conditionTextView;
+    private TextView locationTextView;
+    private AutoCompleteTextView SearchAutoCompleteTextView;
+    private Button searchButton;
 
-    @Override
-    public void serviceSuccess(final Channel channel) {
-    }
-
-    @Override
-    public void serviceFailure(Exception exception) {
-    }
-
-    @Override
-    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-    }
 
     //If user presses the back button, prompt with a dialog
     @Override
@@ -86,5 +86,116 @@ public class MainActivity extends AppCompatActivity implements WeatherServiceCal
         }
         return false;
     }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //checks the internet connection
+        if (internetDialog())
+        {
+            //if there is internet, does not exit the app
+            //starts the app
+
+            /*weatherIconImageView = (ImageView) findViewById(R.id.weatherIconImageView);
+            temperatureTextView = (TextView) findViewById(R.id.temperatureTextView);
+            conditionTextView = (TextView) findViewById(R.id.conditionTextView);
+            locationTextView = (TextView) findViewById(R.id.locationTextView);
+            SearchAutoCompleteTextView = (AutoCompleteTextView) findViewById(R.id.searchWeather);
+            searchButton = (Button) findViewById(R.id.WeatherButton);
+
+
+            SearchAutoCompleteTextView.setAdapter(new GooglePlacesAutocompleteAdapter(getApplicationContext(), R.layout.list_item));
+            SearchAutoCompleteTextView.setOnItemClickListener(this);
+            final String[] locationSearch = new String[1];*/
+
+            service = new YahooWeatherService(this);
+            service.setLocation("Toronto");
+
+//            appendLog(getCurrentTimeStamp());
+
+           /* searchButton.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View v)
+                {
+                    if(SearchAutoCompleteTextView.getText().toString().isEmpty()){
+                        Toast.makeText(getApplicationContext(),"Enter a location",Toast.LENGTH_LONG).show();
+                    }
+                    else
+                    {
+                        locationSearch[0] = SearchAutoCompleteTextView.getText().toString();
+                        service.refreshWeather(locationSearch[0]);
+                        Toast.makeText(getApplicationContext(),service.getLocation(),Toast.LENGTH_LONG).show();
+
+                        //Toast.makeText(getApplicationContext(),locationSearch[0],Toast.LENGTH_LONG).show();
+
+                    }
+                }
+            });*/
+
+        }
+    }
+
+
+    private static String getCurrentTimeStamp(){
+        try {
+
+            SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            String currentDateTime = dateFormat.format(new Date()); // Find todays date
+
+            return currentDateTime;
+        } catch (Exception e) {
+            e.printStackTrace();
+
+            return null;
+        }
+    }
+
+    @Override
+    public void serviceSuccess(final Channel channel) {
+//        appendLog("success");
+//        Item item = channel.getItem();
+//        int resourceId = getResources().getIdentifier("drawable/icon_" + item.getCondition().getCode(),null, getPackageName());
+//
+//        @SuppressWarnings("deprecation") Drawable weatherIconDrawable = getResources().getDrawable(resourceId);
+//
+//        weatherIconImageView.setImageDrawable(weatherIconDrawable);
+//        temperatureTextView.setText(item.getCondition().getTemperature() + "\u00B0" + channel.getUnits().getTemperature());
+//        conditionTextView.setText(item.getCondition().getDescription());
+//        locationTextView.setText(service.getLocation());
+//
+//        DownloadTask task = new DownloadTask();
+//        task.execute(service.getLocation());
+//        Toast.makeText(getApplicationContext(), service.getLocation(), Toast.LENGTH_SHORT).show();
+//        appendLog("weather for: " + service.getLocation() + " : " + channel.getItem().getCondition().getTemperature());
+//        final int tempChange = channel.getItem().getCondition().getTemperature();
+//        int fah = (int) 1.8;
+//        final int fahTemp = tempChange * fah + 32;
+//        temperatureTextView.setOnTouchListener(new View.OnTouchListener() {
+//
+//            @Override
+//            public boolean onTouch(View arg0, MotionEvent arg1) {
+//
+//                if (temperatureTextView.getText().equals(fahTemp+ "\u00B0F")) {
+//                    temperatureTextView.setText(tempChange + "\u00B0" + channel.getUnits().getTemperature());
+//                } else if(temperatureTextView.getText().equals(tempChange+ "\u00B0" + channel.getUnits().getTemperature())) {
+//                    temperatureTextView.setText(fahTemp+ "\u00B0F");
+//                }
+//                //Toast.makeText(getApplicationContext(),tempChange,Toast.LENGTH_LONG).show();
+//
+//                return false;
+//            }
+//        });
+
+    }
+
+    @Override
+    public void serviceFailure(Exception exception) {
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+    }
+
 
 }
--- app/src/main/java/prayashmishra/com/yahooweatherapp/service/YahooWeatherService.java
+++ app/src/main/java/prayashmishra/com/yahooweatherapp/service/YahooWeatherService.java
@@ -23,4 +23,83 @@ public class YahooWeatherService {
     private WeatherServiceCallback callback;
     private String location;
     private Exception error;
+
+    public YahooWeatherService(WeatherServiceCallback callback) {
+        this.callback = callback;
+    }
+    public String getLocation(){
+        return location;
+    }
+    public void setLocation(String location){
+        this.location = location;
+        refreshWeather(location);
+    }
+
+    public void refreshWeather(final String l) {
+        new AsyncTask<String, Void, String>() {
+
+            @Override
+            protected String doInBackground(String... strings) {
+
+                String YQL = String.format("select * from weather.forecast where woeid in (select woeid from geo.places(1) where text=\"%s\") and u= 'c'", strings[0]);
+
+                String endpoint = String.format("https://query.yahooapis.com/v1/public/yql?q=%s&format=json", Uri.encode(YQL));
+
+                try {
+                    URL url = new URL(endpoint);
+                    URLConnection connection = url.openConnection();
+//
+                    InputStream inputStream = connection.getInputStream();
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+                    StringBuilder result = new StringBuilder();
+                    String line;
+
+                    while ((line = reader.readLine()) != null) {
+                        result.append(line);
+                    }
+                    return result.toString();
+
+                } catch (Exception e) {
+                    error = e;
+                }
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(String s) {
+
+                if (s == null && error != null) {
+                    callback.serviceFailure(error);
+                    return;
+                }
+
+                try {
+                    JSONObject data = new JSONObject(s);
+                    JSONObject queryResults = data.optJSONObject("query");
+                    int count = queryResults.optInt("count");
+
+                    if (count == 0) {
+                        callback.serviceFailure(new LocationWeatherException("Could not find weather for" + location));
+                        return;
+                    }
+                    Channel channel = new Channel();
+                    channel.populate(queryResults.optJSONObject("results").optJSONObject("channel"));
+
+                    Log.d("MYINT", "value: " + count);
+
+                    callback.serviceSuccess(channel);
+                } catch (JSONException e) {
+                    callback.serviceFailure(e);
+                }
+
+            }
+        }.execute(location);
+    }
+
+    public class LocationWeatherException extends Exception {
+
+        public LocationWeatherException(String detailMessage) {
+            super(detailMessage);
+        }
+    }
 }
